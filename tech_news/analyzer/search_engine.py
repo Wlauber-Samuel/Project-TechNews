6ed8dd1cf18bdc746930dd16d992c4cab61da91f
Wlauber <wlaubersmuel@gmail.com>
2023-11-09 19:22:15 -0300

    requisito 08 concluído

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,9 @@
 # Requisito 7
+import datetime
+
+from tech_news.database import search_news
+
+
 def search_by_title(title):
     """Seu código deve vir aqui"""
     raise NotImplementedError
@@ -6,8 +11,15 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        date_format = datetime.strptime(date, "%Y-%m-%d")
+        new_format = date_format.strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    response = search_news({"timestamp": new_format})
+    list_result = [(item["title"], item["url"]) for item in response]
+    return list_result
 
 
 # Requisito 9
