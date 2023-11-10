ef2b55029643d9b9c8a8764c85056895e966f5ef
Wlauber <wlaubersmuel@gmail.com>
2023-11-09 21:07:09 -0300

    requisito 10 concluído

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,24 @@
 # Requisito 10
+from tech_news.database import (
+    find_news,
+)
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    response = find_news()
+    if len(response) == 0:
+        return []
+    else:
+        contagens = {}
+    for category in response:
+        if category["category"] in contagens:
+            contagens[category["category"]] += 1
+        else:
+            contagens[category["category"]] = 1
+
+    top_5 = sorted(contagens.items(), key=lambda x: (-x[1], x[0]))
+    top_5 = top_5[:5]
+    array = []
+    for contagem in top_5:
+        array.append(contagem[0])
+    return array
